ffe02644b9a4400efbe37799a125bc848bea5da7
CloudZun <info@cloudzun.com>
2020-04-24 09:43:14 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 az group create --name sockshop --location southeastasia
-az vm create --resource-group sockshop --name mysockshop01 --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_B2ms --custom-data SS.txt  
+az vm create --resource-group sockshop --name mysockshop01 --public-ip-address-dns-name sockshopdemo --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_B2ms --custom-data SS.txt  
 az vm open-port --resource-group sockshop --name mysockshop01 --port 0-65535 --priority 1001
